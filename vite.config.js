0488c7f55487efda9d37031260f96238fa14be2d
Den <khmlden@yandex.ru>
2025-06-29 19:28:42 +0300

    2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ghPages from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/khmelldev-website/",
-  plugins: [react(), ghPages()],
+  base: "/khmelldev/",
+  plugins: [react()],
 });
